6b24eec6cc0ea62c02121438855db3051c76c94c
Daniel Beall <djbeall1101@gmail.com>
2019-04-16 22:53:00 -0400

    Started boss scene

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+	GameObject player;
+	Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		Vector2 dir = player.transform.position - transform.position;
+        rb.AddForce(dir.normalized * 5);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		if(Input.GetKeyDown("space")) {
+			rb.AddForce(Vector2.up * 250 * (transform.localScale.x * 2));
+		}
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb.AddForce(Vector2.right * moveHorizontal * speed * (transform.localScale.x));
+    }
+}
