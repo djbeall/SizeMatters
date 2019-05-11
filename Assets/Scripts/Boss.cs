db522ccf0017c3a772f5e955c4414d1c3b4effbc
Daniel Beall <djbeall1101@gmail.com>
2019-05-11 13:19:22 -0400

    Added a Bit of Boss Intelligence

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -17,8 +17,20 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-		Vector2 dir = player.transform.position - transform.position;
-        rb.AddForce(dir.normalized * 5 * speed);
+        if ((player.transform.position.x > 17 || player.transform.position.x < -7) && transform.position.x < 17 && transform.position.x > -7)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else if (Mathf.Abs(player.transform.position.x - transform.position.x) < 6)
+        {
+            Vector2 dir = player.transform.position - transform.position;
+            rb.AddForce(dir.normalized * 7 * speed);
+        }
+        else
+        {
+            Vector2 dir = player.transform.position - transform.position;
+            rb.AddForce(dir.normalized * 5 * speed);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,7 +39,7 @@ public class Boss : MonoBehaviour
         {
             Vector2 force = transform.position - collision.transform.position;
             force.Normalize();
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-force * (Mathf.Pow(transform.localScale.x, 3)), ForceMode2D.Impulse);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-force * (Mathf.Pow(transform.localScale.x, 3.2f)), ForceMode2D.Impulse);
         }
     }
 }
